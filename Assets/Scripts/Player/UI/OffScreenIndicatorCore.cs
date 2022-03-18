e1de569befad72fa9e5906244bef897ee957b346
RomanNiki <49449001+RomanNiki@users.noreply.github.com>
2022-03-18 15:33:53 +0300

    Added pointer to ball

---
+++ Assets/Scripts/Player/UI/OffScreenIndicatorCore.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Player.UI
+{
+    public static class OffScreenIndicatorCore
+    {
+        public static Vector3 GetScreenPosition(Camera mainCamera, Vector3 targetPosition)
+        {
+            var screenPosition = mainCamera.WorldToScreenPoint(targetPosition);
+            return screenPosition;
+        }
+
+        public static bool IsTargetVisible(Vector3 screenPosition)
+        {
+            var isTargetVisible = screenPosition.z > 0 && screenPosition.x > 0 && screenPosition.x < Screen.width &&
+                                  screenPosition.y > 0 && screenPosition.y < Screen.height;
+            return isTargetVisible;
+        }
+
+        public static void GetArrowIndicatorPositionAndAngle(ref Vector3 screenPosition, ref float angle,
+            Vector3 screenCentre, Vector3 screenBounds)
+        {
+            screenPosition -= screenCentre;
+
+            if (screenPosition.z < 0)
+            {
+                screenPosition *= -1;
+            }
+
+            angle = Mathf.Atan2(screenPosition.y, screenPosition.x);
+            var slope = Mathf.Tan(angle);
+
+            screenPosition = screenPosition.x > 0
+                ? new Vector3(screenBounds.x, screenBounds.x * slope, 0)
+                : new Vector3(-screenBounds.x, -screenBounds.x * slope, 0);
+
+            if (screenPosition.y > screenBounds.y)
+            {
+                screenPosition = new Vector3(screenBounds.y / slope, screenBounds.y, 0);
+            }
+            else if (screenPosition.y < -screenBounds.y)
+            {
+                screenPosition = new Vector3(-screenBounds.y / slope, -screenBounds.y, 0);
+            }
+
+            screenPosition += screenCentre;
+        }
+    }
+}
---
+++ Assets/_Source_Files/Scripts/ParticleDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using Pool.NightPool;
+using UnityEngine;
+
+namespace _Source_Files.Scripts
+{
+    public class ParticleDestroyer : MonoBehaviour, IPoolItem
+    {
+        [SerializeField] private ParticleSystem _particleSystem;
+        private const float LifeTime = 2f;
+
+        private void Despawn()
+        {
+            NightPool.Despawn(this);
+        }
+
+        public void OnSpawn()
+        {
+            var particleSystemMain = _particleSystem.main;
+            particleSystemMain.startColor = Random.ColorHSV();
+            _particleSystem.Play();
+            Invoke(nameof(Despawn), LifeTime);
+        }
+
+        public void OnDespawn()
+        {
+        }
+    }
+}
